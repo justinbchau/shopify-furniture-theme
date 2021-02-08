c547413d6999c8016d0ccaa6a165f3d79edc9247
Justin <justinchau31@gmail.com>
2021-02-08 10:36:36 -0800

    Fixing Footer Positioning

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Header from './header';
-// import Footer from './footer';
+import Footer from './footer';
 
 export default function Layout({ children }) {
   return (
@@ -14,16 +14,7 @@ export default function Layout({ children }) {
 
       <main className='main'>{children}</main>
 
-      <footer className='footer'>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className='logo' />
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,104 @@
+import Link from 'next/link';
+
 export default function Footer() {
+  const date = new Date().getFullYear();
+
   return (
-    <footer className='footer'>
-      <a
-        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Powered by <img src='/vercel.svg' alt='Vercel Logo' className='logo' />
-      </a>
-    </footer>
+    <>
+      <footer className='footer'>
+        <div className='footer__container'>
+          <div className='box'>
+            <h1 className='footer__title'>
+              <Link href='/'>SELENTIAL</Link>
+            </h1>
+            <div className='link-bar__socials'>
+              <a href='#'>
+                <img
+                  src='/facebook.svg'
+                  alt='Dropdown Icon'
+                  className='link-bar__icons'
+                />
+              </a>
+              <a href='#'>
+                <img
+                  src='/twitter.svg'
+                  alt='Dropdown Icon'
+                  className='link-bar__icons'
+                />
+              </a>
+              <a href='#'>
+                <img
+                  src='/instagram.svg'
+                  alt='Dropdown Icon'
+                  className='link-bar__icons'
+                />
+              </a>
+              <a href='#'>
+                <img
+                  src='/youtube.svg'
+                  alt='Dropdown Icon'
+                  className='link-bar__icons'
+                />
+              </a>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2 className='box__title'>More Links</h2>
+            <ul>
+              <li>
+                <a href='/'>My Account</a>
+              </li>
+              <li>
+                <a href='/'>Tack your order</a>
+              </li>
+              <li>
+                <a href='/'>FAQs</a>
+              </li>
+              <li>
+                <a href='/'>Payment Methods</a>
+              </li>
+              <li>
+                <a href='/'>Shipping Guide</a>
+              </li>
+              <li>
+                <a href='/'>Products Support</a>
+              </li>
+              <li>
+                <a href='/'>Gift Card Balance</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='box'>
+            <h2 className='box__title'>Explore the page</h2>
+            <ul>
+              <li>About Us</li>
+              <li>Our Guarantees</li>
+              <li>Terms and Conditions</li>
+            </ul>
+          </div>
+
+          <div className='box'>
+            <h2 className='box__title'>Contact Us</h2>
+            <ul>
+              <li>shanjairaj@gmail.com</li>
+              <li>(272) 471-2477</li>
+            </ul>
+
+            <h2 className='box__title'>Our Location</h2>
+            <ul>
+              <li>2411 Columbia Boulevard Baltimore, MD</li>
+            </ul>
+          </div>
+        </div>
+        <div className='footer__bottom'>
+          <div className='left'>
+            <p>{date} &copy; All rights reserved</p>
+          </div>
+          <div className='right'></div>
+        </div>
+      </footer>
+    </>
   );
 }
